e15bf52a39ce1e06dac341ba8d6350fb6077b074
Vyacheslav Vorobev <voyzvz@gmail.com>
2021-12-01 16:44:58 +0800

    java ee

---
+++ java/JavaEE/java-ee-core/src/main/java/owpk/service/SingletonEJB.java
@@ -0,0 +1,14 @@
+package owpk.service;
+
+//import javax.ejb.Singleton;
+//import javax.json.Json;
+
+//@Singleton
+public class SingletonEJB {
+//    public String buildJson() {
+//        var json = Json.createObjectBuilder();
+//        json.add("name", "1");
+//        json.add("body", "2");
+//        return json.toString();
+//    }
+}
---
+++ java/JavaEE/java-ee-core/src/main/java/owpk/service/SingletonInject.java
@@ -0,0 +1,14 @@
+package owpk.service;
+
+//import javax.inject.Singleton;
+//import javax.json.Json;
+
+//@Singleton
+public class SingletonInject {
+//    public String buildJson() {
+//        var json = Json.createObjectBuilder();
+//        json.add("name", "1");
+//        json.add("body", "2");
+//        return json.toString();
+//    }
+}
