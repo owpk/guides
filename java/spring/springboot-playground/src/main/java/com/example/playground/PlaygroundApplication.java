26986c050c9083d24639c1dbb125333b55ff50e9
Vyacheslav Vorobev <voyzvz@gmail.com>
2022-02-22 00:48:29 +0800

    js

--- java/spring/springboot-playground/src/main/java/com/example/playground/PlaygroundApplication.java
+++ java/spring/springboot-playground/src/main/java/com/example/playground/PlaygroundApplication.java
@@ -2,57 +2,44 @@ package com.example.playground;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.constraints.Pattern;
 
-@SpringBootApplication
-@EnableCaching
 public class PlaygroundApplication implements CommandLineRunner {
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
     static {
         objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
     }
 
-    private CacheManager cacheManager;
-    private Service service;
-
-
     public static void main(String[] args) throws Exception {
-//        SpringApplication.run(PlaygroundApplication.class, args);
         PlaygroundApplication p = new PlaygroundApplication();
         p.run(args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        DataR<Empty> s = new DataR<>();
-        s.setData(new Empty());
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
 
-        String a = objectMapper.writeValueAsString(s);
-        System.out.println(a);
+        Obj o = new Obj();
+        o.a = "aasdA_-";
+
+
+        var err = validator.validate(o);
+        System.out.println(err);
+
     }
 
-    @Autowired
-    public void setCacheManager(CacheManager cacheManager) {
-        this.cacheManager = cacheManager;
+    public static class Obj {
+        @Pattern(regexp = "[a-zA-Z\\-_]+")
+        String a;
     }
-
-    @Autowired
-    public void setService(Service service) {
-        this.service = service;
-    }
-
 
 }
