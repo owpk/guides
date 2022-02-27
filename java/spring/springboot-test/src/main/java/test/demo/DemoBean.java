1aadc9bcc5dcf3402593126dc4cc7e7171113113
Vyacheslav Vorobev <voyzvz@gmail.com>
2022-02-27 19:17:50 +0800

    jee updae

---
+++ java/spring/springboot-test/src/main/java/test/demo/DemoBean.java
@@ -0,0 +1,10 @@
+package test.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Vorobyev Vyacheslav
+ */
+public class DemoBean {
+}
